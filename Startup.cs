315d19e3068d35482549bf7fe5a4c51f16ca1854
Rynco Maekawa <lynzrand@outlook.com>
2019-08-22 16:10:58 +0800

    Update app settings

--- Startup.cs
+++ Startup.cs
@@ -28,7 +28,9 @@ namespace Sojourner
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
+            services.AddMvc()
+                .AddJsonOptions(options => options.UseCamelCasing(false))
+                .SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 
             services.Configure<DbSettings>(Configuration.GetSection("DbSettings"));
             services.AddSingleton<IDbSettings>(settings => settings.GetRequiredService<IOptions<DbSettings>>().Value);
